9caf8ef67df95414878a8f109c0836fba4ac042d
Dieter <dieter.banaag@ucalgary.ca>
2026-07-27 13:49:24 -0600

    fixed url

--- frontend/src/app/services/home-page.service.ts
+++ frontend/src/app/services/home-page.service.ts
@@ -1,8 +1,7 @@
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-import { environment } from '../../../../environments/environment';
 
 export interface HomePageContent {
   id: number;
--- frontend/src/app/services/posts.service.ts
+++ frontend/src/app/services/posts.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { environment } from '../../../../environments/environment';
+import { environment } from './../../environments/environment';
+
 import { Post } from '../models/post.model';
 
 @Injectable({
