26cdc65298b8e7e82ac81a31cf3587c0d195e47d
Dieter <dieter.banaag@ucalgary.ca>
2026-07-27 12:18:58 -0600

    added other sections

--- frontend/src/app/features/home/components/about-melrose/about-melrose.ts
+++ frontend/src/app/features/home/components/about-melrose/about-melrose.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface CityStatistic {
+  value: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-about-melrose',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about-melrose.html',
   styleUrl: './about-melrose.scss',
 })
-export class AboutMelrose {}
+export class AboutMelrose {
+  readonly statistics: CityStatistic[] = [
+    {
+      value: '148,000',
+      label: 'Residents',
+    },
+    {
+      value: '64',
+      label: 'Neighbourhoods',
+    },
+    {
+      value: '120+',
+      label: 'Parks and green spaces',
+    },
+    {
+      value: '1892',
+      label: 'Year established',
+    },
+  ];
+}
